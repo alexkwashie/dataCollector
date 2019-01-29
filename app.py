30f3675c981ae1c1c07161b9d72bbdef29efb23b
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-01-29 22:50:57 +0000

    first section of app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+#render_template is to render the html page
+#request is to request the email and HTTP etc
+
+app = Flask(__name__) #the app script is defaultly named '__main__'
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+def index():
+    return render_template("success.html")
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run(port=5700)
